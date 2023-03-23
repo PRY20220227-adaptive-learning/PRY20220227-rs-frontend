871f49cfa1b7315345202e5454fe7ee45feef57d
Sebastian Alfaro <salfaro@certicom.com.pe>
2023-03-23 16:11:47 -0500

    finished teacher homepage design

--- src/pages/teacher/index.js
+++ src/pages/teacher/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from "../../layouts/Header";
+import ResourceModal from "./ResourceModal"
 
 function Teacher() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedResources, setSelectedResources] = useState([]);
 
   const json = [
     {
@@ -18,7 +22,7 @@ function Teacher() {
           resUrl: "https://www.youtube.com/watch?v=VLj-Dbz-CWk"
         },
         {
-          resName: "PDF - Firts conditional",
+          resName: "PDF - First conditional",
           resUrl: "https://www.youtube.com/watch?v=VLj-Dbz-CWk"
         }
       ]
@@ -61,14 +65,33 @@ function Teacher() {
         }
       ]
     },
+    {
+      studentName: "Balto Alfaro",
+      studentLevel: "Avanzado",
+      learningStyle: "Kinestesico",
+      recResources: [
+        {
+          resName: "Dinámica-Present Simple",
+          resUrl: "https://www.youtube.com/watch?v=agawqrqk"
+        },
+        {
+          resName: "Dinámica-Present Simple",
+          resUrl: "https://www.youtube.com/watch?v=agawqrqk"
+        },
+        {
+          resName: "Dinámica-Present Simple",
+          resUrl: "https://www.youtube.com/watch?v=agawqrqk"
+        }
+      ]
+    },
   ];
 
   return (
     <div className="mx-auto w-full container">
       <Header />
       <h1 className="text-3xl font-semibold text-sky-700 mt-10 mb-5">Resumen</h1>
-      <div className="p-9 mx-20 bg-sky-200 rounded-lg shadow-lg">
-        <div className="grid grid-cols-4 gap-4 py-2 mb-2 bg-white rounded-md">
+      <div className="p-9 mx-20 mb-4 bg-sky-200 rounded-lg shadow-lg">
+        <div className="grid grid-cols-4 gap-4 py-2 mb-4 bg-white rounded-md">
           <div className="text-lg font-bold">Estudiante</div>
           <div className="text-lg font-bold">Nivel de conocimientos</div>
           <div className="text-lg font-bold"> Estilo de aprendizaje</div>
@@ -80,10 +103,21 @@ function Teacher() {
               <div>{item.studentName}</div>
               <div>{item.studentLevel}</div>
               <div>{item.learningStyle}</div>
+              <div>
+                <button onClick={() => { setIsModalOpen(true); setSelectedResources(item.recResources); }}
+                  className="bg-sky-600 p-1.5 rounded-md shadow-md text-white hover:bg-sky-700">
+                  Ver recursos
+                </button>
+              </div>
             </React.Fragment>
           ))}
         </div>
       </div>
+      <ResourceModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        resources={selectedResources}
+      />
     </div>
   );
 }
